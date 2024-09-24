1b32f678173c55d904755f075f36c070cf3be41b
kingno3l <nuelluiz@gmail.com>
2024-09-24 17:07:55 +0100

    this has been implemented, i also implemented named urls

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
+
+app_name = 'articles'  # This defines the namespace
 
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name='list'),  # Add the 'name' attribute here
+    path('<slug:slug>/', views.article_detail, name='detail'),
 ]
